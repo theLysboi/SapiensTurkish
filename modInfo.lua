5aaf2400d9f4b3b0a3f20862f87ce4b0dbfca40b
Lysboi <110478866+theLysboi@users.noreply.github.com>
2023-05-19 11:44:48 +0300

    Translations added and changed with the 0.4 Major Update!

---
+++ modInfo.lua
@@ -0,0 +1,13 @@
+local modInfo = {
+    name = "Turkish Localization",
+    description = "A Turkish localization for the game Sapiens.",
+    preview = "img/logo04.png",
+    version = "0.4",
+    type = "app",
+    developer = "Lysboi",
+    website = "https://github.com/theLysboi",
+    loadOrder = 1,
+
+}
+
+return modInfo
